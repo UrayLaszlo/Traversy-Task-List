dca5dddf06dc393d8f6e92e855e1f79c711a080b
UrayLaszlo <35655671+UrayLaszlo@users.noreply.github.com>
2018-08-30 18:58:40 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear task event
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 
 }
 
@@ -47,4 +53,40 @@ function addTask(e) {
 
 
   e.preventDefault();
+}
+
+// Remove task
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')) {
+      if(confirm('Are you sure?')) {
+        e.target.parentElement.parentElement.remove();
+      }
+  }
+}
+
+// Clear tasks
+function clearTasks() {
+  // taskList.innerHTML = '';
+
+  // Faster
+
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+
+  }
+}
+
+// Filter tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+  
+    document.querySelectorAll('.collection-item').forEach
+      (function(task) {
+      const item = task.firstChild.textContent;
+      if(item.toLowerCase().indexOf(text) != -1){
+        task.style.display = 'block';
+      } else {
+         task.style.display = 'none';
+       }
+     }) ;
 }
